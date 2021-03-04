df654cd0240c98cb3bf57354c62f0f0f1fe9d97c
Anne Felden <amfelden@lake.lbl.gov>
2021-03-04 02:00:11 -0800

    Verbose

--- lib/src/AMRElliptic/AMRPoissonOp.cpp
+++ lib/src/AMRElliptic/AMRPoissonOp.cpp
@@ -2098,9 +2098,12 @@ AMRLevelOp<LevelData<FArrayBox> >* AMRPoissonOpFactory::AMRnewOp(const ProblemDo
   if (ref == 0)
     {
       // coarsest AMR level
-      //ANNA FAS I dunno
-      //if (m_domains.size() == 1)
-      if ((m_domains.size() == 1) || (!m_boxes[1].isClosed()) )
+      // (DFM 1/19/21) Second check handles the case where we allow
+      // for more levels, but this particular solver only has a single level
+      // second check should only get invoked if first one is false, so
+      // shouldn't cause problems if only a single level is allowed
+      // (since in that case there is no m_boxes[1])
+      if (m_domains.size() == 1 || !m_boxes[1].isClosed())
         {
           // no finer level
           newOp->define(m_boxes[0], m_dx[0],
