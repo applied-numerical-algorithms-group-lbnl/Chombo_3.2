a22ba10833501e6ba9875a99ddbec601645413c5
dtgraves <dtgraves@lbl.gov>
2022-11-01 15:11:03 -0700

    progress on trasition amrpoisson code

--- releasedExamples/Proto/AMRPoisson/poissonSolve.cpp
+++ releasedExamples/Proto/AMRPoisson/poissonSolve.cpp
@@ -19,130 +19,23 @@
 #include "BRMeshRefine.H"
 #include "LoadBalance.H"
 #include "ProblemDomain.H"
-#include "BCFunc.H"
-#include "AMRPoissonOp.H"
-#include "AMRMultiGrid.H"
-#include "BiCGStabSolver.H"
-#include "BoxIterator.H"
+
 #include "CONSTANTS.H"
 #include "memusage.H"
 #include "Proto.H"
+#include "ProtoTestCommon.H"
 
-namespace Chombo
-{
-  //just group stuff the same way that Proto does
-  class AMRGrid
-  {
-  public:
-    Vector<DisjointBoxLayout> m_grids;
-    Vector<int>               m_refRat;
-  }
-}
-
-
-/**
-  Begin bits pirated from Proto/examples and Proto/tests
- **/
-
-AMRGrid telescopingGrid(
-        Point crseDomainSize,
-        unsigned int numLevels,
-        std::vector<Point> refRatios,
-        std::vector<Point>   boxSizes,
-        std::array<bool, DIM> periodicity)
-{
-    std::vector<DisjointBoxLayout> layouts;
-    layouts.resize(numLevels);
-    Box domainBox(crseDomainSize);
-    ProblemDomain domain(crseDomainSize, periodicity);
-    layouts[0].define(domain, domainBox, boxSizes[0]);
-    for (int lvl = 1; lvl < numLevels; lvl++)
-    {
-        domain = domain.refine(refRatios[lvl-1]);
-        domainBox = domainBox.grow(-domainBox.sizes()/4).refine(refRatios[lvl-1]);
-        layouts[lvl].define(domain, domainBox, boxSizes[lvl]); 
-    }
-    return AMRGrid(layouts, refRatios, numLevels);
-}
-
-AMRGrid telescopingGrid(
-        int domainSize,
-        unsigned int numLevels,
-        Point refRatio,
-        Point boxSize)
-{
-    std::vector<Point> refRatios(numLevels-1, refRatio);
-    std::vector<Point> boxSizes(numLevels, boxSize);
-    Point crseDomainSize = Point::Ones(domainSize);
-    std::array<bool, DIM> periodicity;
-    periodicity.fill(true);
-    return telescopingGrid(crseDomainSize, numLevels, refRatios, boxSizes, periodicity);
-}
-
-template<typename T, MemType MEM = MEMTYPE_DEFAULT>
-class BoxOp_Laplace : public BoxOp<T, 1, 0, MEM>
-{
-    public:
-
-    // These functions are not optional
-    inline static Point ghost() { return Point::Ones(2);}
-    inline static Point auxGhost() { return Point::Zeros();}
-    inline static constexpr int order() { return 4; }
-
-    inline BoxOp_Laplace() : BoxOp<T, 1, 0, MEM>() {}
-    inline BoxOp_Laplace(double a_dx) : BoxOp<T, 1, 0, MEM>(a_dx) {}
-    inline BoxOp_Laplace(std::array<double, DIM> a_dx) : BoxOp<T, 1, 0, MEM>(a_dx) {}
-
-    inline double spectralRadius() const {return (2.0*DIM) / pow(this->dxMin(), 2); }
-    inline void flux(
-            BoxData<T, 1, MEM>& a_flux,
-            const BoxData<T, 1, MEM>& a_state,
-            int a_dir) const
-    {
-        auto GRAD = Stencil<T>::DiffCellToFace(a_dir, Side::Lo);
-        //auto GRAD = 1.0*Shift::Zeros() - 1.0*Shift::Basis(a_dir, -1);
-        a_flux |= GRAD(a_state, 1.0 / this->dx()[a_dir]); 
-    }
-};
-
-typedef Chombo::FArrayBox                                     ch_fab;
-typedef Chombo::LevelData<ch_fab>                             ch_leveldata;
-typedef Chombo::DisjointBoxLayout                             ch_dbl;
-typedef Chombo::Vector<ch_dbl>                                ch_hierarchy;
-typedef Chombo::ProblemDomain                                 ch_probdom;
-                                                              
-typedef  Proto::AMRGrid                                       pr_amrgrid;
-typedef  Proto::ProblemDomain                                 pr_probdom;
-typedef  Proto::BoxOp_Laplace<double>                         pr_poissonop;
-typedef  Proto::AMRSolver_FASMultigrid<pr_poissonop, double>  pr_amrsolver;
 
 
 /**
 End pirated bits.   Back to ChomboLand.
 **/
-shared_ptr<pr_amrgrid>  > 
-getProtoAMRGrid()
-{
-  unsigned int domsize, boxsize, refrat, numLevels, pt;
-  ParmParse pp(amrgrid);
-  pp.get("domain_size"     , domsize);
-  pp.get("fixed_box_size"  , boxsize);
-  pp.get("refinement_ratio". refirat);
-
-  ///Proto defines the grid hierarchy
-  Point refiRatPt = Point::Ones(refirat);
-  Point boxSizePt = Point::Ones(boxsize);
-  shared_ptr<pr_amrgrid>
-    retval(new  telescopingGrid(domsize, numLevels, refRatPt, boxSizePt));
-  return retval;
-}
   
 shared_ptr<pr_amr_poisson>  >
-getAMRPoissonSolver(const shared_ptr<pr_amrgrid>& a_amrgrid)
+getAMRPoissonSolver(const shared_ptr<pr_amrgrid>& pr_hierarchy)
 {
-  vector<int> refrat;
-  int errcode = getAMRGrids(hierarchy, refrat);
-  CH_assert(errcode == 0);
+  const auto& hierarchy = *pr_hierarchy;
+  unsigned int domsize = hierarchy[0].problemDomain.sizes()[0];
   Real dx  = 1./Real(domsize);
   std::array<double, DIM> dxarray;
   dxarray.fill(dx);
@@ -153,15 +46,23 @@ getAMRPoissonSolver(const shared_ptr<pr_amrgrid>& a_amrgrid)
   
   return retval;
 }
-
+/**
+ **/
 int runFASSolver()
 {
   //Proto has to drive the grid generation
-  shared_ptr<pr_amrgrid> = getProtoAMRGrid();
-  shared_ptr<ch_amrgrid> 
+  //get the grid hierarchy
+  shared_ptr<pr_amr_grid>   pr_hierarchy =  getProtoAMRGrid();
+  //get the chombo equivalent of the grid hierarchy
+  shared_ptr<ch_amr_grid>   ch_hierarchy =  getChomboAMRGrid(pr_hierarchy);
+  
+  //get the poisson solver
+  shared_ptr<pr_amr_solver> amrpoisPtr = getAMRPoissonSolver(pr_hierarchy);
   
 }
-/*****/
+
+/**
+ **/
 int main(int argc, char* argv[])
 {
 #ifdef CH_MPI
