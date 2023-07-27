19d152c02d893a8891b95650120ca511d69b0a89
dtgraves <dtgraves@lbl.gov>
2023-07-27 14:06:32 -0700

    exchangeMode was breaking AMRPoissonOp.

--- lib/src/AMRElliptic/AMRPoissonOp.cpp
+++ lib/src/AMRElliptic/AMRPoissonOp.cpp
@@ -264,13 +264,15 @@ void AMRPoissonOp::residualI(LevelData<FArrayBox>&       a_lhs,
 {
   CH_TIME("AMRPoissonOp::residualI");
 
+  
   LevelData<FArrayBox>& phi = (LevelData<FArrayBox>&)a_phi;
-  if (s_exchangeMode == 0)
-    phi.exchange(phi.interval(), m_exchangeCopier);
-  else if (s_exchangeMode == 1)
-    phi.exchangeNoOverlap(m_exchangeCopier);
-  else
-    MayDay::Abort("exchangeMode");
+  phi.exchange();
+//  if (s_exchangeMode == 0)
+//    phi.exchange(phi.interval(), m_exchangeCopier);
+//  else if (s_exchangeMode == 1)
+//    phi.exchangeNoOverlap(m_exchangeCopier);
+//  else
+//    MayDay::Abort("exchangeMode");
 
   const DisjointBoxLayout& dbl = a_lhs.disjointBoxLayout();
   DataIterator dit = phi.dataIterator();
@@ -347,12 +349,13 @@ void AMRPoissonOp::applyOpI(LevelData<FArrayBox>&       a_lhs,
   CH_TIME("AMRPoissonOp::applyOpI");
 
   LevelData<FArrayBox>& phi = (LevelData<FArrayBox>&)a_phi;
-  if (s_exchangeMode == 0)
-    phi.exchange(phi.interval(), m_exchangeCopier);
-  else if (s_exchangeMode == 1)
-    phi.exchangeNoOverlap(m_exchangeCopier);
-  else
-    MayDay::Abort("exchangeMode");
+  phi.exchange();
+//  if (s_exchangeMode == 0)
+//    phi.exchange(phi.interval(), m_exchangeCopier);
+//  else if (s_exchangeMode == 1)
+//    phi.exchangeNoOverlap(m_exchangeCopier);
+//  else
+//    MayDay::Abort("exchangeMode");
 
   const DisjointBoxLayout& dbl = a_lhs.disjointBoxLayout();
   DataIterator dit = phi.dataIterator();
@@ -656,13 +659,14 @@ void AMRPoissonOp::restrictResidual(LevelData<FArrayBox>&       a_resCoarse,
 
   homogeneousCFInterp(a_phiFine);
 
-  if (s_exchangeMode == 0)
-    a_phiFine.exchange(a_phiFine.interval(), m_exchangeCopier);
-  else if (s_exchangeMode == 1)
-    a_phiFine.exchangeNoOverlap(m_exchangeCopier);
-  else
-    MayDay::Abort("exchangeMode");
+//  if (s_exchangeMode == 0)
+//    a_phiFine.exchange(a_phiFine.interval(), m_exchangeCopier);
+//  else if (s_exchangeMode == 1)
+//    a_phiFine.exchangeNoOverlap(m_exchangeCopier);
+//  else
+//    MayDay::Abort("exchangeMode");
 
+  a_phiFine.exchange();
   const DisjointBoxLayout& dblFine = a_phiFine.disjointBoxLayout();
   DataIterator dit = a_phiFine.dataIterator();
   int nbox=dit.size();
@@ -1240,12 +1244,13 @@ void AMRPoissonOp::levelGSRB( LevelData<FArrayBox>&       a_phi,
 
       {
         CH_TIME("AMRPoissonOp::levelGSRB::exchange");
-        if (s_exchangeMode == 0)
-          a_phi.exchange( a_phi.interval(), m_exchangeCopier );
-        else if (s_exchangeMode == 1)
-          a_phi.exchangeNoOverlap(m_exchangeCopier);
-        else
-          MayDay::Abort("exchangeMode");
+//        if (s_exchangeMode == 0)
+//          a_phi.exchange( a_phi.interval(), m_exchangeCopier );
+//        else if (s_exchangeMode == 1)
+//          a_phi.exchangeNoOverlap(m_exchangeCopier);
+//        else
+//          MayDay::Abort("exchangeMode");
+        a_phi.exchange();
       }
 #pragma omp parallel
       {
