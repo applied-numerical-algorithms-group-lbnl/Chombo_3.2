d04ca6db5f23aa502387e61e7af6d066ef0f0995
dtgraves <dtgraves@lbl.gov>
2023-08-10 13:51:00 -0700

    more cleaning

--- releasedExamples/TimeParallel/colored_solves/colored_solves.cpp
+++ releasedExamples/TimeParallel/colored_solves/colored_solves.cpp
@@ -162,7 +162,6 @@ int
 runSolver(const DisjointBoxLayout  & a_grid,
           const ProblemDomain      & a_domain,
           const Real               & a_dx,
-          MPI_Comm  a_comm,
           int       a_icolor) //included in file names if positive
 {
   CH_TIME("runSolver");
@@ -207,39 +206,8 @@ runSolver(const DisjointBoxLayout  & a_grid,
   bool forceHomogeneous = false;
   amrSolver->solve(phi, rhs, finestLevel, 0,
                    zeroInitialGuess, forceHomogeneous, print);
-  MPI_Barrier(a_comm);
-  MPI_Barrier(Chombo_MPI::comm);
 
-#ifdef CH_USE_HDF5
   
-  string phi_fname("4586.hdf5");
-  string rhs_fname("4586.hdf5");
-
-  //negative signals world
-  if(a_icolor >= 0)
-  {
-    phi_fname = string("phi_color") + to_string(a_icolor) + string("_chombo.hdf5");
-    rhs_fname = string("rhs_color") + to_string(a_icolor) + string("_chombo.hdf5");
-  }
-  else
-  {
-    phi_fname = string("phi_world") +  string("_chombo.hdf5");
-    rhs_fname = string("rhs_world") +  string("_chombo.hdf5");
-  }
-    
-
-  Vector<string> rhs_vars(1, string("rhs"));
-  Vector<string> phi_vars(1, string("phi"));
-  double time = 0;  double dt = 0; //just used for labeling
-  
-  pout() << "runSolver: writing charge   (rhs) to " << rhs_fname << endl;
-  Box dombox= a_domain.domainBox();
-  WriteAMRHierarchyHDF5(rhs_fname, amrGrids, rhs, rhs_vars, dombox, a_dx, dt, time, refRatios, 1);
-  pout() << "runSolver: writing solution (phi) to " << phi_fname << endl;
-  WriteAMRHierarchyHDF5(phi_fname, amrGrids, phi, phi_vars, dombox, a_dx, dt, time, refRatios, 1);
-
-#endif 
-
   pout() << "runSolver: clean up " << endl;
   for (int lev=0; lev<phi.size(); lev++)
   {
@@ -248,144 +216,6 @@ runSolver(const DisjointBoxLayout  & a_grid,
   }
   delete amrSolver;
 
-  return 0;
-}
-/**
-   just to get something working
-   taken unedited from mpitutorial.com
-**/
-int
-mpiTutorialCommSplitTest()
-{
-  // Get the rank and size in the original communicator
-  int world_rank, world_size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-  int color = world_rank / 4; // Determine color based on row
-
-// Split the communicator based on the color and use the
-// original rank for ordering
-  MPI_Comm row_comm;
-  MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &row_comm);
-
-  int row_rank, row_size;
-  MPI_Comm_rank(row_comm, &row_rank);
-  MPI_Comm_size(row_comm, &row_size);
-
-//  printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\n",
-//         world_rank, world_size, row_rank, row_size);
-  //chombofied output so this can be run at large concurrencies.
-  pout() << "mpi_tutorial: world rank = " << world_rank << endl;
-  pout() << "mpi_tutorial: world size = " << world_size << endl;
-  pout() << "mpi_tutorial: row   rank = " <<   row_rank << endl;
-  pout() << "mpi_tutorial: row   size = " <<   row_size << endl;
-  MPI_Comm_free(&row_comm);
-
-  return 0;
-}
-///
-/**
-   Taking the above tutorial and chombofying it.
-**/
-int
-chomboCommSplitTest()
-{
-  ParmParse pp("chomboCommSplitTest");
-  int nx            = 4586;
-  int maxBoxSize    = 4586;
-  int procsPerColor = 4586;
-  pp.get("nx", nx);
-  pp.get("maxBoxSize", maxBoxSize);
-  pp.get("procsPerColor" , procsPerColor);
-  ///
-  // Get the rank and size in the original communicator
-  int world_rank, world_size;
-  MPI_Comm_rank(Chombo_MPI::comm, &world_rank);
-  MPI_Comm_size(Chombo_MPI::comm, &world_size);
-
-  // Determine color based on original rank
-  int icolor = world_rank / procsPerColor; 
-
-  MPI_Comm color_comm;
-  MPI_Comm_split(Chombo_MPI::comm, icolor, world_rank, &color_comm);
-  
-  int color_rank, color_size;
-  MPI_Comm_rank(color_comm, &color_rank);
-  MPI_Comm_size(color_comm, &color_size);
-  
-  pout() << "chomboCommSplit: world rank = " << world_rank << endl;
-  pout() << "chomboCommSplit: world size = " << world_size << endl;
-  pout() << "chomboCommSplit: color rank = " << color_rank << endl;
-  pout() << "chomboCommSplit: color size = " << color_size << endl;
-  pout() << "chomboCommSplit: proc color = " << icolor     << endl;
-  
-
-  IntVect ivlo =        IntVect::Zero;
-  IntVect ivhi = (nx-1)*IntVect::Unit;
-  Box domain(ivlo, ivhi);
-  Vector<Box> boxes;
-  Vector<int> procs;
-  domainSplit(domain, boxes,  maxBoxSize);
-  LoadBalance(procs, boxes);
-  DisjointBoxLayout dblWorld(boxes, procs, Chombo_MPI::comm);
-  DisjointBoxLayout dblColor(boxes, procs, color_comm);
-
-  
-  return 0;
-}
-
-///
-/**
-   Taking the above tutorial and chombofying it.
-**/
-int
-handleOpenTest()
-{
-  int procsPerColor = 4586;
-  ParmParse pp("handleOpenTest");
-  pp.get("procsPerColor" , procsPerColor);
-  ///
-  // Get the rank and size in the original communicator
-  int world_rank, world_size;
-  MPI_Comm_rank(Chombo_MPI::comm, &world_rank);
-  MPI_Comm_size(Chombo_MPI::comm, &world_size);
-
-  // Determine color based on original rank
-  int icolor = world_rank / procsPerColor; 
-
-  MPI_Comm color_comm;
-  MPI_Comm_split(Chombo_MPI::comm, icolor, world_rank, &color_comm);
-  
-  int color_rank, color_size;
-  MPI_Comm_rank(color_comm, &color_rank);
-  MPI_Comm_size(color_comm, &color_size);
-  
-  pout() << "handleOpenTest: world rank = " << world_rank << endl;
-  pout() << "handleOpenTest: world size = " << world_size << endl;
-  pout() << "handleOpenTest: color rank = " << color_rank << endl;
-  pout() << "handleOpenTest: color size = " << color_size << endl;
-  pout() << "handleOpenTest: proc color = " << icolor     << endl;
-  
-  string world_name("world_comm.hdf5");
-  string color_name = string("color") + to_string(icolor) + string("_comm.hdf5");
-  if(1)
-  {
-    pout() << "world handle open starting for filename " << world_name << endl;
-    HDF5Handle world_handle(world_name.c_str(),  HDF5Handle::CREATE, "Chombo_Global");
-    pout() << "world handle open closing " << endl;
-    world_handle.close();
-    pout() << "world handle leaving scope " << endl;
-  }
-  if(1)
-  {
-    pout() << "color handle open starting for filename " << color_name << endl;
-    HDF5Handle color_handle(color_name.c_str(),  HDF5Handle::CREATE, "Chombo_Global");
-    pout() << "color handle open closing " << endl;
-    color_handle.close();
-    pout() << "color handle leaving scope " << endl;
-  }
-  
   return 0;
 }
 
@@ -447,61 +277,19 @@ runColoredSolvers()
       {
         pout() << "isolve = " << isolve << endl;
         // negative color indicates world communicator (for I/O)
-        runSolver(dblWorld, domain, dx,  Chombo_MPI::comm, -4586);  
+        runSolver(dblWorld, domain, dx,  -4586);  
       }
     }
     pout() << "runColoredSolvers: running colored solvers" << endl;
     {
       CH_TIME("colored solves");
-      runSolver(dblColor, domain, dx,  color_comm, icolor);
+      runSolver(dblColor, domain, dx,   icolor);
     }
   }
 
   return 0;
 }
 
-  
-/// drives the tests here
-int spmdTest()
-{
-  if(0)
-  {
-    int ierr = handleOpenTest();
-    if( ierr != 0)
-    {
-      cerr << " handleOpenTest retuned error code "  << ierr;
-      return ierr;
-    }
-  }
-  if(0)
-  {
-    int ierr = mpiTutorialCommSplitTest();
-    if( ierr != 0)
-    {
-      cerr << " mpiTutorialCommSplitTest retuned error code "  << ierr;
-      return ierr;
-    }
-  }
-  if(0)
-  { 
-    int ierr = chomboCommSplitTest();
-    if( ierr != 0)
-    {
-      cerr << " mpiTutorialCommSplitTest retuned error code "  << ierr;
-      return 10*ierr;
-    }
-  }
-  if(1)
-  { 
-    int ierr = runColoredSolvers();
-    if( ierr != 0)
-    {
-      cerr << " runColoredSolversTest retuned error code "  << ierr;
-      return 100*ierr;
-    }
-  }
-  return 0;
-}
 
 /// init mpi, init parmparse.  call test.  dump timers. finalize mpi.
 int main(int argc, char* argv[])
@@ -522,10 +310,10 @@ int main(int argc, char* argv[])
   ParmParse  pp(argc-2,argv+2,NULL,in_file);
 
   /// call test
-  status = spmdTest();
+  status = runColoredSolvers();
   if(status != 0)
   {
-    cerr << "spmdTest returned error code " << status << endl;
+    cerr << "runColoredSolvers returned error code " << status << endl;
     return status;
   }
   
