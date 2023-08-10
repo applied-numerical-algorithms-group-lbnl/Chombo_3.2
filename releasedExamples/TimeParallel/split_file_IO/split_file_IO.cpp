5ef176d825d0bee7f6fbde2b077b2d22d2269e21
dtgraves <dtgraves@lbl.gov>
2023-08-10 14:28:59 -0700

    reorganized time_parallel examples seem to work

--- releasedExamples/TimeParallel/split_file_IO/split_file_IO.cpp
+++ releasedExamples/TimeParallel/split_file_IO/split_file_IO.cpp
@@ -28,7 +28,79 @@
 #include "BoxIterator.H"
 #include "CH_HDF5.H"
 
+////
+int
+writeColorFileFromColor(const DisjointBoxLayout  & a_grid,
+                        const ProblemDomain      & a_domain,
+                        const Real               & a_dx,
+                        int       a_icolor) //included in file names if positive
+{
+  string phi_fname("4586.hdf5");
+  //negative signals world
+  if(a_icolor >= 0)
+  {
+    phi_fname = string("phi_color") + to_string(a_icolor) + string("_chombo.hdf5");
+  }
+  else
+  {
+    pout() << "writeColorFromColor: invalid color" << endl;
+    return -4586;
+  }
+  ///packaging up stuff so I can call the most general write function
+  Vector<DisjointBoxLayout> amrGrids(1, a_grid);
+  Vector<ProblemDomain>   amrDomains(1, a_domain);
+  Vector<Real> amrDx(1, a_dx);
+  Vector<int> refRatios(1, 2);
 
+  double time = 0;  double dt = 0; //just used for labeling
+  Vector<string> phi_vars(1, string("phi"));
+  Box dombox= a_domain.domainBox();
+  LevelData<FArrayBox> philev(a_grid, 1, IntVect::Zero);
+  DataIterator dit = a_grid.dataIterator();
+  //have to set it to something
+  Real value = 4586/Real(a_icolor + 1);
+  for(int ibox = 0; ibox < dit.size(); ibox++)
+  {
+    philev[dit[ibox]].setVal(value);
+  }
+  Vector<LevelData<FArrayBox> * > phi(1, &philev);
+  pout() << "writeColorFromColor:  writing phi to " << phi_fname << endl;
+  WriteAMRHierarchyHDF5(phi_fname, amrGrids, phi, phi_vars, dombox, a_dx, dt, time, refRatios, 1);
+
+  return 0;
+}
+
+////
+int
+readColorFileIntoWorld(int       a_icolor) 
+{
+  string phi_fname("4586.hdf5");
+  //negative signals world
+  if(a_icolor >= 0)
+  {
+    phi_fname = string("phi_color") + to_string(a_icolor) + string("_chombo.hdf5");
+  }
+  else
+  {
+    pout() << "readColorIntoWorld: invalid color" << endl;
+    return -4586;
+  }
+  pout() << "readColorIntoWorld: reading" << endl;
+
+  Vector<DisjointBoxLayout>       grids;
+  Vector<LevelData<FArrayBox>* >  phi;
+  Vector<string>                  var_names;
+  Box                             domain;
+  Real                            dx, dt, time;
+  Vector<int>                     refRatio;
+  int                             numLevels;
+
+  ReadAMRHierarchyHDF5( phi_fname,
+                        grids,    phi, var_names, domain,   
+                        dx, dt, time, refRatio,  numLevels);
+                     
+  return 0;
+}
 
 
 int
@@ -67,7 +139,7 @@ writeAndReadData()
   
   int color_rank, color_size;
   MPI_Comm_rank(color_comm, &color_rank);
-  MPI_Comm_size(color_comm, &color_size);
+  MPI_Comm_size(color_comm, &color_size);              // 
   
   pout() << "writeAndReadData: world rank = " << world_rank << endl;
   pout() << "writeAndReadData: world size = " << world_size << endl;
@@ -78,9 +150,15 @@ writeAndReadData()
 
   DisjointBoxLayout dblWorld(boxes, procs, Chombo_MPI::comm);
   DisjointBoxLayout dblColor(boxes, procs, color_comm);
-  writeColorFileFromColor(dblColor, domain, dx,   icolor);
-  readColorFileIntoWorld( dblWorld, domain, dx,   icolor);
-
+  {
+    int eekwrite = writeColorFileFromColor(dblColor, domain, dx,   icolor);
+    if(!eekwrite) return eekwrite;
+  }
+  {
+    //read makes its own dbl
+    int eekread  = readColorFileIntoWorld(icolor);
+    if(!eekread) return eekread;
+  }
   return 0;
 }
 
