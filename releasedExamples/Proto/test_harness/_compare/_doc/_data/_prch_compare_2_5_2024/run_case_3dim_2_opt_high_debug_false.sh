b959aabd0f428e0ba73cc1439a0043f61796b1d9
dtgraves <dtgraves@lbl.gov>
2024-02-06 16:00:28 -0800

    Timers and documents and print statements (oh my!)

---
+++ releasedExamples/Proto/test_harness/_compare/_doc/_data/_prch_compare_2_5_2024/run_case_3dim_2_opt_high_debug_false.sh
@@ -0,0 +1,18 @@
+#/usr/bin/csh
+
+ pushd /home/graves/_git_chombo_3.2/Chombo_3.2/releasedExamples/Proto/test_harness/_compare/_prch_compare_2_5_2024/case_3dim_2_opt_high_debug_false/amr_conductivity; source batch_4586.sh; popd 
+;
+ pushd /home/graves/_git_chombo_3.2/Chombo_3.2/releasedExamples/Proto/test_harness/_compare/_prch_compare_2_5_2024/case_3dim_2_opt_high_debug_false/_old_conductivity; source batch_4586.sh; popd 
+;
+ pushd /home/graves/_git_chombo_3.2/Chombo_3.2/releasedExamples/Proto/test_harness/_compare/_prch_compare_2_5_2024/case_3dim_2_opt_high_debug_false/amr_helmholtz; source batch_4586.sh; popd 
+;
+ pushd /home/graves/_git_chombo_3.2/Chombo_3.2/releasedExamples/Proto/test_harness/_compare/_prch_compare_2_5_2024/case_3dim_2_opt_high_debug_false/_old_helmholtz; source batch_4586.sh; popd 
+;
+ pushd /home/graves/_git_chombo_3.2/Chombo_3.2/releasedExamples/Proto/test_harness/_compare/_prch_compare_2_5_2024/case_3dim_2_opt_high_debug_false/amr_viscous_tensor; source batch_4586.sh; popd 
+;
+ pushd /home/graves/_git_chombo_3.2/Chombo_3.2/releasedExamples/Proto/test_harness/_compare/_prch_compare_2_5_2024/case_3dim_2_opt_high_debug_false/_old_viscous_tensor; source batch_4586.sh; popd 
+;
+ pushd /home/graves/_git_chombo_3.2/Chombo_3.2/releasedExamples/Proto/test_harness/_compare/_prch_compare_2_5_2024/case_3dim_2_opt_high_debug_false/amr_resistivity; source batch_4586.sh; popd 
+;
+ pushd /home/graves/_git_chombo_3.2/Chombo_3.2/releasedExamples/Proto/test_harness/_compare/_prch_compare_2_5_2024/case_3dim_2_opt_high_debug_false/_old_resistivity; source batch_4586.sh; popd 
+;
