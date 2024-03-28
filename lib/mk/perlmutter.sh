53b69a99ec485314a0c239c819ab0498db2786e7
dtgraves <dtgraves@lbl.gov>
2024-03-28 15:49:16 -0700

    got at least one PrCh example to compile on perlmutter

---
+++ lib/mk/perlmutter.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+module unload cmake
+module unload cudatoolkit
+module unload cray-hdf5-parallel
+module unload craype-accel-nvidia80
+
+module load cmake
+module load cudatoolkit
+module load cray-hdf5-parallel
+module load craype-accel-nvidia80
+
